5fac761a84336312037c3d53ff4f657cf0d0e460
Floyd Hightower <floyd.hightower27@gmail.com>
2021-09-14 21:32:11 -0400

    Updating interuptors

--- bible-cross-ref-service/src/service.clj
+++ bible-cross-ref-service/src/service.clj
@@ -1,18 +1,39 @@
 (ns service
-  (:require [io.pedestal.http :as http]
+  (:require [clojure.data.json :as json]
             [io.pedestal.http.route :as route]
             [refs :refer [get-cross-refs]]))
 
 (defn ok [body]
   {:status 200 :body body})
 
-(defn not-found []
-  {:status 404 :body "Not found\n"})
-
 (defn get-search-results [request]
-  (let [query (get-in request [:query-params :q])]
-    (ok (str (count (get-cross-refs query))))))
+  )
+    ;; (ok (get-cross-refs query))))
+
+(def get-search-results
+  {:name ::get-search-results
+   :enter (fn [context]
+            (let [query (get-in context [:request :query-params :q])]
+              (assoc context :response {:body (get-cross-refs query)})))})
+
+(def format-results
+  {:name ::format-results
+   :enter (fn [context]
+            (let [body (get-in context [:response :body])]
+              (update-in context [:response :body] (TODO: CONVERT ARRAY TO MAP HERE...))))})
+
+(def echo
+  {:name ::echo
+   :enter (fn [context]
+            (println (str "a: " (:response context)))
+            context)})
+
+(def jsonify
+  {:name ::jsonify
+   :leave (fn [context]
+            (update context :response (json/write-str (get-in context [:response :body]))))})
 
 (def routes
   (route/expand-routes
-   #{["/search" :get get-search-results :route-name :search]}))
+   #{["/search" :get [jsonify echo get-search-results format-results] :route-name :search]
+     ["/echo" :get echo]}))
