590fa28c436d360bee997a6c29e7bcd89feea1b4
Floyd Hightower <floyd.hightower27@gmail.com>
2021-09-24 07:19:00 -0400

    Simlifying collection and display of results

--- bible-cross-ref-service/src/service.clj
+++ bible-cross-ref-service/src/service.clj
@@ -1,8 +1,11 @@
 (ns service
   (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
             [io.pedestal.http.route :as route]
-            [refs :refer [get-cross-refs]]))
+            [refs :refer [get-cross-refs]]
+            [selmer.parser :as selmer]))
 
+;; todo: use a ring response instead of this function
 (defn ok [body]
   {:status 200 :body body})
 
@@ -13,12 +16,18 @@
                   cross-refs (get-cross-refs query)]
               (assoc context :response (ok cross-refs))))})
 
-(def get-status
-  {:name ::get-status
-   :enter (fn [context]
-            (assoc context :response (ok {:status "Good"})))})
+(defn show-home
+  [request]
+  (ok
+    (selmer/render-file "html/home.html"
+      (if (= (:request-method request) "get")
+        {}
+        (let [query (get-in request [:params "query"])]
+          {:results (get-cross-refs query)
+          :query query})))))
 
 (def routes
   (route/expand-routes
-   #{["/search" :get [http/json-body get-search-results] :route-name :search]
-     ["/status" :get [http/json-body get-status] :route-name :status]}))
+   #{["/" :get [http/html-body show-home] :route-name :home-get]
+     ["/" :post [http/html-body (body-params/body-params) show-home] :route-name :home-post]
+     ["/api/search" :get [http/json-body get-search-results] :route-name :api-search]}))
