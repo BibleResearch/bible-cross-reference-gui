ecc4e95c17887a1c6b47c8bf39d1be2d100333f2
Floyd Hightower <floyd.hightower27@gmail.com>
2021-10-04 19:28:48 -0400

    Adding project.clj

---
+++ bible-cross-ref-service/project.clj
@@ -0,0 +1,23 @@
+(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+  :description "Demo Clojure web app"
+  :url "http://clojure-getting-started.herokuapp.com"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+    [org.clojure/data.csv "1.0.0"]
+    [io.pedestal/pedestal.service "0.5.7"]
+    [io.pedestal/pedestal.route "0.5.7"]
+    [io.pedestal/pedestal.jetty "0.5.7"]
+    [ns-tracker/ns-tracker "0.4.0"]
+    [org.slf4j/slf4j-simple "1.7.28"]
+    [selmer/selmer "1.12.6"]]
+  :min-lein-version "2.0.0"
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :resource-paths ["resources"]
+  :target-path "target/%s/"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "bible-cross-ref-service.jar"
+  :profiles {:production {:env {:production true}}
+             :uberjar {:aot :all}})
